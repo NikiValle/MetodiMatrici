416d5158b4d73f9618bee87405cca5fed435eff7
Niki <valle.nicola.studente@itispaleocapa.it>
2024-04-12 13:21:23 +0200

    Prima versione metodo

---
+++ src/UnoZeroVersioneDue.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class UnoZeroVersioneDue {
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        int[][] mxn = new int[4][4];
+        for(int i=0; i<mxn.length;i++){
+            for (int j=0;j<mxn[0].length;j++){
+                System.out.println("Inserisci un elemento");
+                mxn[i][j]=in.nextInt();
+            }
+        }
+        mxn=ZeroUno(mxn);
+        for(int i=0; i<mxn.length;i++) {
+            for (int j = 0; j < mxn[0].length; j++) {
+                System.out.print(mxn[i][j]+"  ");
+            }
+            System.out.println();
+        }
+    }
+    private static int[][] ZeroUno(int[][]a){
+        for(int i=0;i<a.length;i++){
+            for(int j=i;j<a.length;j++){
+                a[i][j]=1;
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<i+1;j++){
+                a[i][j]=1;
+            }
+        }
+        return a;
+    }
+}
