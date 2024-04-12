90800e3fb283052cdaf292b606f4e66acde635f2
Niki <valle.nicola.studente@itispaleocapa.it>
2024-04-12 13:23:03 +0200

    Versione finale metodo

--- src/UnoZeroVersioneDue.java
+++ src/UnoZeroVersioneDue.java
@@ -24,9 +24,9 @@ public class UnoZeroVersioneDue {
                 a[i][j]=1;
             }
         }
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<i+1;j++){
-                a[i][j]=1;
+        for(int i=1;i<a.length;i++){
+            for(int j=0;j<i;j++){
+                a[i][j]=0;
             }
         }
         return a;
