843335058f1a1b6b82166f9be8724781544de7f3
Niki <valle.nicola.studente@itispaleocapa.it>
2024-04-12 12:51:50 +0200

    Progetto concluso

--- src/SommaColonneMatrice.java
+++ src/SommaColonneMatrice.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 public class SommaColonneMatrice{
     public static void main(String[]args){
         Scanner in = new Scanner(System.in);
-        int[][] mxn = new int[4][4];
+        int[][] mxn = new int[5][4];
         for(int i=0; i<mxn.length;i++){
             for (int j=0;j<mxn[0].length;j++){
                 System.out.println("Inserisci un elemento");
                 mxn[i][j]=in.nextInt();
             }
         }
-        System.out.println(SommaRighe(mxn));
+        System.out.println(SommaColonne(mxn));
     }
-    private static int [] SommaRighe(int[][]a){
+    private static int [] SommaColonne(int[][]a){
         int somma;
         int [] sommato = new int[a.length];
         for(int i=0;i<sommato.length;i++){
             somma =0;
             for(int j=0;j<a[0].length;j++){
-                somma = somma + a[i][j];
+                somma = somma + a[j][i];
             }
         }
         return sommato;
